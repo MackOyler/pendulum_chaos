f350288ed209a371d355199cf0fe083d0b65b7f3
Mack Oyler <120589371+MackOyler@users.noreply.github.com>
2024-06-20 18:20:32 -0500

    ++ simulate_simple_pendulum

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from scipy.integrate import solve_ivp
 import matplotlib.animation as animation
 
 #constants
-g = 9.81 #acceleration due to gravity, in m/s^2
+g = 9.81  # acceleration due to gravity, in m/s^2
+L = 1.0   # length of the pendulum, in meters
+theta0 = np.pi / 4  # initial angle (in radians)
+omega0 = 0.0  # initial angular velocity
 
 #single pendulum simulation first
 def simple_pendulu(t, y):
