e9d13e00b15db7d5a5b1502a318063bb30c7de4d
Mack Oyler <120589371+MackOyler@users.noreply.github.com>
2024-06-20 18:13:25 -0500

    first single/simple pendulum funct

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 import matplotlib.animation as animation
 
+#constants
+g = 9.81 #acceleration due to gravity, in m/s^2
+
+#single pendulum simulation first
+def simple_pendulu(t, y):
+    theta, omega = y
+    dydt = [omega, -g / L * np.sin(theta)]
+    return dydt
